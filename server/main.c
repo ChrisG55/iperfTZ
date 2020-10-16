ChrisG55/iperfTZ

commit 396a8c8b67c3b2eb481efc11bf135d98f0f00871

Add an argument to use UDP protocol
Signed-off-by: ChrisG55 <christian_g_55@msn.com>

server/main.c
@@ -38,6 +38,7 @@ struct args {
   size_t blksize;
   size_t socket_bufsize;
   unsigned long int transmit_bytes;
+  unsigned int protocol;
 };
 
 static void init_args(struct args *args)
@@ -45,6 +46,7 @@ static void init_args(struct args *args)
   args->blksize = TCP_WINDOW_DEFAULT;
   args->socket_bufsize = TCP_WINDOW_DEFAULT;
   args->transmit_bytes = 0;
+  args->protocol = ISPERF_TCP;
 }
 
 static char *init_buffer(struct args *args)
@@ -63,13 +65,16 @@ static int parse_args(struct args *args,
   int c;
   int errflg = 0;
 
-  while ((c = getopt(argc, argv, "l:n:w:")) != -1) {
+  while ((c = getopt(argc, argv, "l:n:uw:")) != -1) {
     switch (c) {
     case 'l':
       args->blksize = strtoul(optarg, (char **)NULL, 10);
       break;
     case 'n':
       args->transmit_bytes = strtoul(optarg, (char **)NULL, 10);
+      break;
+    case 'u':
+      args->protocol = ISPERF_UDP;
       break;
     case 'w':
       args->socket_bufsize = strtoul(optarg, (char **)NULL, 10);
@@ -85,7 +90,7 @@ static int parse_args(struct args *args,
   }
   if (errflg) {
     errno = EINVAL;
-    fprintf(stderr, "usage: %s -l size -n size -w size\n", argv[0]);
+    fprintf(stderr, "usage: %s -l size -n size -u -w size\n", argv[0]);
     return EINVAL;
   }
 

ta/include/iperfTZ_ta.h
@@ -29,6 +29,11 @@ enum cmd_id {
   IPERFTZ_TA_SEND
 };
 
+enum protocol {
+  ISPERF_TCP,
+  ISPERF_UDP
+};
+
 #define ISPERF_ADDRSTRLEN 46
 #define TCP_WINDOW_DEFAULT (16 * 1024)
 
@@ -38,6 +43,7 @@ struct iptz_args {
   uint32_t bitrate;
   uint32_t transmit_bytes;
   char ip[ISPERF_ADDRSTRLEN];
+  uint32_t protocol;
 };
 
 struct iptz_results {

ca/main.c
@@ -49,6 +49,7 @@ static void init_args(struct iptz_args *args)
 {
   args->blksize = TCP_WINDOW_DEFAULT;
   args->socket_bufsize = TCP_WINDOW_DEFAULT;
+  args->protocol = ISPERF_TCP;
 }
 
 static int parse_args(struct iptz_args *args,
@@ -58,7 +59,7 @@ static int parse_args(struct iptz_args *args,
   int c;
   int errflg = 0;
   
-  while ((c = getopt(argc, argv, "b:i:l:n:w:")) != -1) {
+  while ((c = getopt(argc, argv, "b:i:l:n:uw:")) != -1) {
     switch (c) {
     case 'b':
       args->bitrate = strtoul(optarg, (char **)NULL, 10);
@@ -71,6 +72,9 @@ static int parse_args(struct iptz_args *args,
       break;
     case 'n':
       args->transmit_bytes = strtoul(optarg, (char **)NULL, 10);
+      break;
+    case 'u':
+      args->protocol = ISPERF_UDP;
       break;
     case 'w':
       args->socket_bufsize = strtoul(optarg, (char **)NULL, 10);
@@ -90,7 +94,7 @@ static int parse_args(struct iptz_args *args,
   }
   if (errflg) {
     errno = EINVAL;
-    fprintf(stderr, "usage: %s -b size -i IP -l size -n size -w size\n", argv[0]);
+    fprintf(stderr, "usage: %s -b size -i IP -l size -n size -u -w size\n", argv[0]);
     return EINVAL;
   }
 

commit 5c0e3162de54d1431cc25da38a77ba98b3709bff

Fix incorrect labeling in macros.
Some macros were not correctly relabeled and still refer to the old prototype name. This commit properly relabels these macros.

Signed-off-by: ChrisG55 <christian_g_55@msn.com>
Fixes: e420a3535b1643add04bf7a9528571884a90760e
Fixes: 396a8c8b67c3b2eb481efc11bf135d98f0f00871
Fixes: e7e19b4761f336775ce29eb01017eb8025dd7ae2
Fixes: 82604b8878f6b62d48e46e2b21cfcdc27ecabc8b
Fixes: 3429c64f3e8e6d603e5ae09cd7f51170414f5780

server/main.c
@@ -67,7 +67,7 @@ static void init_args(struct args *args)
   args->blksize = TCP_WINDOW_DEFAULT;
   args->socket_bufsize = TCP_WINDOW_DEFAULT;
   args->transmit_bytes = 0;
-  args->protocol = ISPERF_TCP;
+  args->protocol = IPERFTZ_TCP;
   args->reverse = 0;
   args->bitrate = 0;
 }
@@ -110,7 +110,7 @@ static int parse_args(struct args *args,
       args->reverse = 1;
       break;
     case 'u':
-      args->protocol = ISPERF_UDP;
+      args->protocol = IPERFTZ_UDP;
       break;
     case 'w':
       args->socket_bufsize = strtoul(optarg, (char **)NULL, 10);
@@ -274,7 +274,7 @@ static int socket_setup(struct args *args, int *sockfd, int *connection)
   struct sockaddr_in server_addr;
   in_port_t port = 5002;
 
-  if (args->protocol == ISPERF_UDP)
+  if (args->protocol == IPERFTZ_UDP)
     sock_type = SOCK_DGRAM;
   
   *sockfd = socket(AF_INET, sock_type, 0);
@@ -283,7 +283,7 @@ static int socket_setup(struct args *args, int *sockfd, int *connection)
     return *sockfd;
   }
 
-  if (args->protocol == ISPERF_TCP) {
+  if (args->protocol == IPERFTZ_TCP) {
     if (setsockopt(*sockfd, SOL_SOCKET, SO_RCVBUF, &args->socket_bufsize, sizeof(args->socket_bufsize)) == -1) {
       perror("setsockopt");
       return -1;
@@ -300,7 +300,7 @@ static int socket_setup(struct args *args, int *sockfd, int *connection)
     return -1;
   }
 
-  if (args->protocol == ISPERF_TCP) {
+  if (args->protocol == IPERFTZ_TCP) {
     if (tcp_connect(&server_addr, connection, *sockfd) != 0)
       return -1;
     fd = *connection;
@@ -445,7 +445,7 @@ int main(int argc, char *argv[])
   if (rc != 0)
     goto cleanup;
   
-  if (args.protocol == ISPERF_TCP) {
+  if (args.protocol == IPERFTZ_TCP) {
     if (args.reverse == 0)
       rc = tcp_recv(&args, connection, buffer);
     else
@@ -461,7 +461,7 @@ int main(int argc, char *argv[])
   
  cleanup:
   free(buffer);
-  if (args.protocol == ISPERF_TCP)
+  if (args.protocol == IPERFTZ_TCP)
     close(connection);
   close(sockfd);
   

ta/include/iperfTZ_ta.h
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef IPERFTZ_TA_H
-#define ITZPERF_TA_H
+#define IPERFTZ_TA_H
 
 #define IPERFTZ_TA_UUID \
 	{ 0xe649d2ad, 0x543f, 0x4220, \
@@ -30,11 +30,11 @@ enum cmd_id {
 };
 
 enum protocol {
-  ISPERF_TCP,
-  ISPERF_UDP
+  IPERFTZ_TCP,
+  IPERFTZ_UDP
 };
 
-#define ISPERF_ADDRSTRLEN 46
+#define IPERFTZ_ADDRSTRLEN 46
 #define TCP_WINDOW_DEFAULT (16 * 1024)
 
 struct iptz_args {
@@ -42,7 +42,7 @@ struct iptz_args {
   uint32_t socket_bufsize;
   uint32_t bitrate;
   uint32_t transmit_bytes;
-  char ip[ISPERF_ADDRSTRLEN];
+  char ip[IPERFTZ_ADDRSTRLEN];
   uint32_t protocol;
   uint32_t reverse;
 };